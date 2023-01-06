63091bd14523653016f0234c125910f90503b081
GoToGuy91 <minseong91@gmail.com>
2023-01-06 14:06:48 +0900

    [Chore] server 개발환경 세팅

---
+++ server/src/test/java/mainproject/MainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
