1575bfda57e40806dfc65b8c94e268b77b877257
mmyona <hyon2001@ajou.ac.kr>
2023-01-11 21:23:02 +0900

    #10 - feat : 유저 정보 형식 구현

---
+++ client/src/components/WriterInfo.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import Avatar from "./atoms/Avatar";
+
+const WriterInfoContainer = styled.div`
+	width: 140px;
+	height: 30px;
+	border: 1px solid #000000;
+
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+
+	font-family: "Inter";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 8px;
+	line-height: 10px;
+`;
+
+const WriterInfo = ({ name, date }) => {
+	return (
+		<>
+			<WriterInfoContainer>
+				<Avatar />
+				<div>{name || "name"}</div>
+				<div>{date || "2023-01-11"}</div>
+			</WriterInfoContainer>
+		</>
+	);
+};
+
+export default WriterInfo;
