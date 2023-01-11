0f943e42e29ef6acf8086816535f65678026fcf4
mmyona <hyon2001@ajou.ac.kr>
2023-01-11 20:29:00 +0900

    #10 - feat : 카테고리(우리동네/운동 ...) 구현

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,2 +1,47 @@
 // MainCategory
 // SelectCategory
+import theme from "../settings/theme";
+import { ThemeProvider } from "styled-components";
+import styled from "styled-components";
+import Btn from "./atoms/Button";
+
+const SelectCategoryContainer = styled.div`
+	width: 360px;
+	height: 86px;
+	padding: 3px;
+`;
+
+const SelectCategory = () => {
+	return (
+		<ThemeProvider theme={theme}>
+			<SelectCategoryContainer>
+				<Btn
+					btnText={"우리 동네"}
+					width={"171px"}
+					background={theme.color.gray}
+					color={theme.color.navy}
+				/>
+				<Btn
+					btnText={"운동"}
+					width={"171px"}
+					background={theme.color.gray}
+					color={theme.color.navy}
+				/>
+				<Btn
+					btnText={"규칙적인 생활"}
+					width={"171px"}
+					background={theme.color.gray}
+					color={theme.color.navy}
+				/>
+				<Btn
+					btnText={"기타"}
+					width={"171px"}
+					background={theme.color.gray}
+					color={theme.color.navy}
+				/>
+			</SelectCategoryContainer>
+		</ThemeProvider>
+	);
+};
+
+export default SelectCategory;
