90529d6f17c96773bd1e43fa2e7fc3b59e541072
GoToGuy91 <minseong91@gmail.com>
2023-01-12 19:18:01 +0900

    #24 - feat: 엔티티 간 1차 매핑

--- server/src/main/java/mainproject/global/category/Category.java
+++ server/src/main/java/mainproject/global/category/Category.java
@@ -3,15 +3,5 @@ package mainproject.global.category;
 import lombok.Getter;
 
 public enum Category {
-    우리동네("우리동네"),
-    운동("운동"),
-    생활("규칙적인 생활"),
-    기타("기타");
-
-    @Getter
-    private final String names;
-
-    Category(String names) {
-        this.names = names;
-    }
+    우리동네, 운동, 생활, 기타
 }
