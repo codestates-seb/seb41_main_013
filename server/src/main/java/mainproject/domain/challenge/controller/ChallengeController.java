c6d539c3fff54755145d23abb3ab1c1ad0ef44dc
GoToGuy91 <minseong91@gmail.com>
2023-01-20 09:01:39 +0900

    #34 - feat: API 문서화 - Challenge

--- server/src/main/java/mainproject/domain/challenge/controller/ChallengeController.java
+++ server/src/main/java/mainproject/domain/challenge/controller/ChallengeController.java
@@ -1,5 +1,8 @@
 package mainproject.domain.challenge.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import mainproject.domain.challenge.dto.ChallengePostDto;
 import mainproject.domain.challenge.dto.ChallengeResponseDto;
 import mainproject.domain.challenge.entity.Challenge;
@@ -20,6 +23,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api/challenges")
 @Validated
+@Api(tags = "챌린지 생성, 조회, 삭제")
 public class ChallengeController {
     private final ChallengeService challengeService;
     private final ChallengeMapper mapper;
@@ -30,8 +34,10 @@ public class ChallengeController {
     }
 
     // 챌린지 생성
+    @ApiOperation(value = "챌린지 생성")
     @PostMapping
-    public ResponseEntity postChallenge(@RequestBody ChallengePostDto request) {
+    public ResponseEntity postChallenge(@ApiParam(name = "챌린지 상세정보 입력", value = postChallengeDescription, required = true)
+                                            @RequestBody ChallengePostDto request) {
         Challenge challenge = challengeService.createChallenge(mapper.challengePostDtoToChallenge(request));
 
         ChallengeResponseDto response = mapper.challengeToChallengeResponseDto(challenge);
@@ -39,9 +45,21 @@ public class ChallengeController {
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
+    final String postChallengeDescription = "hostMemberId: 회원번호 (회원 등록 후 챌린지 생성 가능)" + "\r\n" +
+            "category: 카테고리 (우리동네, 운동, 생활, 기타 중 입력)" + "\r\n" +
+            "title: 챌린지 제목 (50자까지 입력 가능) " + "\r\n" +
+            "content: 챌린지 설명 (500자까지 입력 가능) " + "\r\n" +
+            "startAt: 챌린지 시작 날짜 예) 2023-02-01 (내일 이후부터 선택 가능)" + "\r\n" +
+            "endAt: 챌린지 종료 날짜 (시작 날짜 이후로 설정 가능)" + "\r\n" +
+            "frequency: 인증 빈도 (생략 가능, default = 매일)" + "\r\n" +
+            "snapshotStartAt: 인증 시작 시간 예) 00:00 (생략 가능, default = 00:00:00)" + "\r\n" +
+            "snapshotEndAt: 인증 종료 시간 (시작 시간 이후로 설정 가능, 생략 가능, default = 23:59:59)";
+
     // 챌린지 목록 최신순 조회
+    @ApiOperation(value = "챌린지 목록 최신순 조회")
     @GetMapping("/new")
-    public ResponseEntity getNewChallenges(@RequestParam @Nullable Category category) {
+    public ResponseEntity getNewChallenges(@ApiParam(name = "카테고리 선택", value = "미선택시 전체 카테고리에서 조회", required = false)
+                                               @RequestParam @Nullable Category category) {
         List<Challenge> challenges = challengeService.findNewChallenges(category);
 
         List<ChallengeResponseDto> response = mapper.challengesToChallengeResponseDtos(challenges);
@@ -51,8 +69,10 @@ public class ChallengeController {
     }
 
     // 챌린지 목록 참여자순 조회
+    @ApiOperation(value = "챌린지 목록 참여자순 조회")
     @GetMapping("/hot")
-    public ResponseEntity getHotChallenges(@RequestParam @Nullable Category category) {
+    public ResponseEntity getHotChallenges(@ApiParam(name = "카테고리 선택", value = "미선택시 전체 카테고리에서 조회", required = false)
+                                               @RequestParam @Nullable Category category) {
         List<Challenge> challenges = challengeService.findHotChallenges(category);
 
         List<ChallengeResponseDto> response = mapper.challengesToChallengeResponseDtos(challenges);
@@ -61,8 +81,10 @@ public class ChallengeController {
     }
 
     // 회원이 생성한 챌린지 조회
+    @ApiOperation(value = "회원이 생성한 챌린지 조회")
     @GetMapping("/{member-id}")
-    public ResponseEntity getCreateChallenges(@PathVariable("member-id") @Positive long memberId) {
+    public ResponseEntity getCreateChallenges(@ApiParam(name = "회원번호 입력", required = true)
+                                                  @PathVariable("member-id") @Positive long memberId) {
         List<Challenge> challenges = challengeService.findCreateChallenges(memberId);
 
         List<ChallengeResponseDto> response = mapper.challengesToChallengeResponseDtos(challenges);
@@ -71,8 +93,10 @@ public class ChallengeController {
     }
 
     // 챌린지 검색(제목+내용)
+    @ApiOperation(value = "챌린지 검색(제목+내용)")
     @GetMapping
-    public ResponseEntity getSearchChallenges(@RequestParam @NotNull(message = "검색어를 입력하세요.")
+    public ResponseEntity getSearchChallenges(@ApiParam(name = "검색어", value = "100자까지 입력 가능", required = true)
+                                                  @RequestParam @NotNull(message = "검색어를 입력하세요.")
                                                   @Size(max = 100, message = "검색어는 100자까지 입력 가능합니다.")
                                                   String query) {
         List<Challenge> challenges = challengeService.searchChallenges(query);
@@ -83,8 +107,10 @@ public class ChallengeController {
     }
 
     // 챌린지 삭제
+    @ApiOperation(value = "챌린지 삭제")
     @DeleteMapping("/{challenge-id}")
-    public ResponseEntity deleteChallenge(@PathVariable("challenge-id") @Positive long challengeId) {
+    public ResponseEntity deleteChallenge(@ApiParam(name = "챌린지번호 입력", required = true)
+                                              @PathVariable("challenge-id") @Positive long challengeId) {
         challengeService.deleteChallenge(challengeId);
 
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
--- server/src/main/java/mainproject/domain/challenge/dto/ChallengePostDto.java
+++ server/src/main/java/mainproject/domain/challenge/dto/ChallengePostDto.java
@@ -1,5 +1,7 @@
 package mainproject.domain.challenge.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import mainproject.domain.challenge.entity.Frequency;
 import mainproject.domain.member.entity.Member;
@@ -14,8 +16,10 @@ import java.time.format.DateTimeFormatter;
 public class ChallengePostDto {
     @NotNull
     @Positive
+    @ApiModelProperty(required = true, example = "1")
     private long hostMemberId;
 
+    @ApiModelProperty(hidden = true)
     public Member getMember() {
         Member member = new Member();
         member.setId(hostMemberId);
@@ -23,38 +27,52 @@ public class ChallengePostDto {
     }
 
     @NotNull(message = "카테고리를 선택하세요.")
+    @ApiModelProperty(required = true, example = "우리동네")
     private Category category;
 
     @NotBlank(message = "제목을 입력하세요.")
     @Size(max = 50, message = "제목은 50자까지 입력 가능합니다.")
+    @ApiModelProperty(required = true, example = "같이 운동해요~")
     private String title;
 
     @NotBlank(message = "설명을 입력하세요.")
     @Size(max = 500, message = "설명은 500자까지 입력 가능합니다.")
+    @ApiModelProperty(required = true, example = "우리동네에서 운동 후 하루에 한 번 인증사진을 등록하시면 됩니다.")
     private String content;
 
     // private Image challengeImage;  // TODO: 이미지파일
 
     @NotNull(message = "시작 날짜를 선택하세요.")
     //@Future(message = "시작 날짜는 내일 이후부터 선택 가능합니다.") // 챌린지 상태변화 테스트시 주석 처리
+    @ApiModelProperty(required = true, example = "2023-02-01")
+    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
     private LocalDate startAt;
 
     @NotNull(message = "종료 날짜를 선택하세요.")
+    @ApiModelProperty(required = true, example = "2023-12-31")
+    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
     private LocalDate endAt;
 
     @AssertTrue(message = "종료 날짜는 시작 날짜 이후부터 선택 가능합니다.")
+    @ApiModelProperty(hidden = true)
     public boolean isValidChallengePeriod() {
         return endAt.isAfter(startAt);
     }
 
     @NotNull(message = "인증 빈도를 선택하세요.")
+    @ApiModelProperty(required = false, example = "매일")
     private Frequency frequency = Frequency.매일; // 기본값 - 매일
-    
+
+    @ApiModelProperty(required = false, example = "00:00")
+    @JsonFormat(pattern = "HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalTime snapshotStartAt = LocalTime.parse("00:00:00", DateTimeFormatter.ofPattern("HH:mm:ss"));  // 기본값 - 00:00:00
 
+    @ApiModelProperty(required = false, example = "23:59")
+    @JsonFormat(pattern = "HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalTime snapshotEndAt = LocalTime.parse("23:59:59", DateTimeFormatter.ofPattern("HH:mm:ss"));    // 기본값 - 23:59:59
 
     @AssertTrue(message = "종료 시간은 시작 시간 이후부터 선택 가능합니다.")
+    @ApiModelProperty(hidden = true)
     public boolean isValidSnapshotTime() {
         return snapshotEndAt.isAfter(snapshotStartAt);
     }
