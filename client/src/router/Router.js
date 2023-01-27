506be71a028f8613780ea5cbdf6ff83f7a89e8ec
jjeonge97 <cowguswjd@gmail.com>
2023-01-27 23:03:06 +0900

    #81-remove:비밀번호찾기/비밀번호 변경 페이지 삭제

--- client/src/router/Router.js
+++ client/src/router/Router.js
@@ -15,11 +15,9 @@ import MyChallengeOthers from "../pages/MyChallengeOthers";
 import { MyPage } from "../pages/MyPage";
 import { UserCreateChallenge } from "../pages/UserCreateChallenge";
 import { UserCompleteChallenge } from "../pages/UserCompleteChallenge";
-import { UserPasswordChange } from "../pages/UserPasswordChange";
 import { UserProfileEdit } from "../pages/UserProfileEdit";
 import { SignUp } from "../pages/SignUp";
 import { SignIn } from "../pages/SignIn";
-import { FindPassword } from "../pages/FindPassword";
 //community
 import { Community } from "../pages/Community";
 import { CommunityCategoryBoard } from "../pages/CommunityCategoryBoard";
@@ -44,7 +42,6 @@ export const OurPath = () => {
 			<Route element={<Overlaps hasHeader={false} hasFooter={false} />}>
 				<Route path="/userCreate" element={<UserCreateChallenge />} />
 				<Route path="/userComplete" element={<UserCompleteChallenge />} />
-				<Route path="/changePw" element={<UserPasswordChange />} />
 				<Route path="/editProfile" element={<UserProfileEdit />} />
 				<Route path="/challenges/:categoryId" element={<HomeCategoryBoard />} />
 				<Route
@@ -79,7 +76,6 @@ export const OurPath = () => {
 			<Route element={<Overlaps hasHeader={true} hasFooter={false} />}>
 				<Route path="/signup" element={<SignUp />} />
 				<Route path="/login" element={<SignIn />} />
-				{/* <Route path="/findPw" element={<FindPassword />} /> */}
 			</Route>
 		</Routes>
 		// </Suspense>
