a89a91e949b8c34cee893f580cb9c4d56c79cb9c
soojiin <skim69068@gmail.com>
2023-01-28 17:38:34 +0900

    #66 - feat: 마이챌린지 무한스크롤 제거

--- client/src/pages/MyChallenge.js
+++ client/src/pages/MyChallenge.js
@@ -1,40 +1,41 @@
 import styled from "styled-components";
 import { BackToTopBtn, CreateBtn } from "../components/Button";
 import { MyChallengeItem } from "../components/ChallengeItem";
-import { Loading } from "../components/Loading";
+import { Logout } from "../components/Logout";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import InfiniteScroll from "react-infinite-scroll-component";
+import { useSelector } from "react-redux";
+import { NoDataDiv } from "../components/NoData";
 
 const MyChallenge = () => {
 	const [challenges, setChallenges] = useState([]);
-	const [page, setPage] = useState(1);
-	const [hasMoreData, setHasMoreData] = useState(true);
+	const [hasData, setHasData] = useState(true);
+  const isLogin = useSelector((state) => state.loginStatus.status);
 
-	const memberId = 1;
+	const { memberId, accessToken } = useSelector(
+		(state) => state.loginUserInfo.loginUserInfo,
+	);
 
 	useEffect(() => {
 		getMyChallengesList();
 	}, []);
 
 	const getMyChallengesList = async () => {
-		if (!hasMoreData) return;
 		try {
 			const response = await axios.get(
-				`${process.env.REACT_APP_SERVER_URL}/api/challengers/${memberId}/challenging?page=${page}`,
+				`${process.env.REACT_APP_SERVER_URL}/api/challengers/${memberId}/challenging`,
+				{
+					headers: {
+						Authorization: `Bearer ${accessToken}`,
+					},
+					withCredentials: true,
+				},
 			);
-			if (response.data.length < 10) {
-				setHasMoreData(false);
-			}
-			setChallenges([...challenges, ...response.data]);
+			if (response.data.length === 0) { setHasData(false); }
+			setChallenges(response.data);
 		} catch (error) {
 			console.error(error);
 		}
-	};
-
-	const loadMoreData = () => {
-		setPage(page + 1);
-		getMyChallengesList();
 	};
 
   const categoryId = {
@@ -44,49 +45,66 @@ const MyChallenge = () => {
 		"기타": "3",
 	};
 
-	return (
-		<MyChallengeWrapper>
-			<InfiniteScroll
-				className="infinite-scroll"
-				dataLength={challenges.length}
-				next={loadMoreData}
-				hasMore={hasMoreData}
-				loader={<Loading />}
-			>
-				{challenges.map((challenge) => (
-					<MyChallengeItem
-						// imgUrl={challenge.imageUrl}
-						challengeTitle={challenge.title}
-						challengerNum={challenge.challengerCount}
-						challengeFrequency={challenge.frequency}
-						challengeDate={`${challenge.StartAt} - ${challenge.EndAt}`}
-						challengeTime={`${challenge.snapshotStartAt} - ${challenge.snapshotEndAt}`}
-						// progress={challenge.progress}
-						// NavTo={`/challenges/${categoryId[challenge.category]}/${challenge.challengeId}`}
+  return (
+    <Wrapper>
+      {isLogin ?
+			 	 hasData ? (
+        <MyChallengeWrapper>
+					<MyChallengeItemContainer>
+						{challenges.map((challenge) => (
+							<MyChallengeItem
+								imgUrl={challenge.imageUrl}
+								challengeTitle={challenge.title}
+								challengerNum={challenge.challengerCount}
+								challengeFrequency={challenge.frequency}
+								challengeDate={`${challenge.StartAt} - ${challenge.EndAt}`}
+								challengeTime={`${challenge.snapshotStartAt} - ${challenge.snapshotEndAt}`}
+								progress={challenge.progress}
+								NavTo={`/challenges/${categoryId[challenge.category]}/${challenge.challengeId}`}
+							/>))}
+					</MyChallengeItemContainer>
+					<CreateBtn NavTo="/challenges/create" />
+					<BackToTopBtn />
+        </MyChallengeWrapper>
+       ) 
+			 :
+			 (
+				<>
+					<NoDataDiv
+						text="등록된 마이챌린지"
 					/>
-				))}
-			</InfiniteScroll>
-			<CreateBtn NavTo="/challenges/create" />
-			<BackToTopBtn />
-		</MyChallengeWrapper>
-	);
+					<CreateBtn NavTo="/challenges/create" />
+				</>
+			 )
+			 : (
+        <Logout />
+      )}
+    </Wrapper>
+  );
 };
 
-const MyChallengeWrapper = styled.div`
-	margin-top: 5.2rem;
-	margin-bottom: 6.5rem;
-	/* height: 100%; */
+const Wrapper = styled.div`
+	position: absolute;
+	left: 0;
+	top: 5.2rem;
+	bottom: 6.5rem;
+	overflow-y: scroll;
+	width: 100%;
+	padding: 0 1.3rem;
+	/* padding-bottom: 2.5rem; */
 
-	& .infinite-scroll {
-		display: flex;
-		flex-wrap: wrap;
-		align-items: center;
-		justify-content: center;
-
-		::-webkit-scrollbar {
+	::-webkit-scrollbar {
 			display: none;
 		}
-	}
+`;
+
+const MyChallengeItemContainer = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+`;
+
+const MyChallengeWrapper = styled.div`
+  /* height: 100%; */
 `;
 
 export default MyChallenge;
