0342915aaa1738a6588abfa1ecaeec00f54d65c3
GoToGuy91 <minseong91@gmail.com>
2023-01-29 21:36:14 +0900

    #65 - feat: 기존 구현내용 보완 - 이미지 업로드 방식 수정(presignedURL)

--- server/src/main/java/mainproject/domain/image/controller/ImageController.java
+++ server/src/main/java/mainproject/domain/image/controller/ImageController.java
@@ -1,11 +1,15 @@
 package mainproject.domain.image.controller;
 
 import com.amazonaws.HttpMethod;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -18,9 +22,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.time.Instant;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/upload")
@@ -42,15 +49,60 @@ public class ImageController {
 
         ImageResponseDto response = mapper.imageToImageResponseDto(image);
 
+        File convertedFile = convertFile(file);
+
+        response.setUrl(upload(convertedFile));
+
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    // MultipartFile을 File로 변환
+    private File convertFile(MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+        if (convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+
+            return Optional.of(convertFile)
+                    .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환 실패"));
+        }
+
+        return new File("");
+    }
+
+    // 파일 등록, presignedURL 리턴, 파일 삭제
+    private String upload(File convertedFile) {
+        String bucketName = "s3://bucket-deploy-challenge";
+        //String bucketName = "http://bucket-deploy-challenge.s3-website.ap-northeast-2.amazonaws.com";
+        String fileName = "/" + convertedFile.getName();
+
+        AmazonS3Client amazonS3Client = new AmazonS3Client();
+        amazonS3Client.putObject(
+                new PutObjectRequest(bucketName, fileName, convertedFile).withCannedAcl(CannedAccessControlList.PublicRead)
+        );
+
+        removeFile(convertedFile);
+
+        return amazonS3Client.getUrl(bucketName, fileName).toString();
+    }
+
+    // 파일 삭제
+    private void removeFile(File convertedFile) {
+        if (convertedFile.delete()) {
+            System.out.println("파일이 삭제되었습니다.");
+        }
+        else {
+            System.out.println("파일이 삭제되지 못했습니다.");
+        }
     }
 
     @ApiOperation(value = "presignedURL 획득")
     @GetMapping
-    public static String generatePresignedUrl() {
+    public String generatePresignedUrl() {
         Regions clientRegion = Regions.DEFAULT_REGION;
-        String bucketName = "bucket-deploy-challenge";
-        String objectKey = "s3://bucket-deploy-challenge";
+        String bucketName = "s3://bucket-deploy-challenge";
+        String objectKey = "test.txt";
 
         AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
                 .withRegion(clientRegion)
@@ -67,7 +119,7 @@ public class ImageController {
         System.out.println("Generating pre-signed URL.");
         GeneratePresignedUrlRequest generatePresignedUrlRequest =
                 new GeneratePresignedUrlRequest(bucketName, objectKey)
-                        .withMethod(HttpMethod.GET)
+                        .withMethod(HttpMethod.PUT)
                         .withExpiration(expiration);
         URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
 
