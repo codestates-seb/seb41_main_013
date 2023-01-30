eafea0c8c3b89ab578758d707dab721c2a5413c3
GoToGuy91 <minseong91@gmail.com>
2023-01-30 22:13:20 +0900

    #65 - feat: 기존 구현내용 보완 - presignedURL 이미지 업로드 방식 구현

--- server/src/main/java/mainproject/domain/image/controller/ImageController.java
+++ server/src/main/java/mainproject/domain/image/controller/ImageController.java
@@ -24,6 +24,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.Instant;
 import java.util.Date;
@@ -32,6 +33,12 @@ import java.util.Date;
 @RequestMapping(value = "/api/upload")
 @Api(tags = "이미지파일 업로드")
 public class ImageController {
+    @Value("${AWS_ACCESS_KEY}")
+    private String accessKey;
+
+    @Value("${AWS_SECRET_KEY}")
+    private String secretKey;
+
     private final ImageService imageService;
     private final ImageMapper mapper;
 
@@ -40,7 +47,43 @@ public class ImageController {
         this.mapper = mapper;
     }
 
-    @ApiOperation(value = "이미지파일 업로드")
+    @ApiOperation(value = "1. 이미지를 버킷에 업로드하기 위한 presignedURL 발급")
+    @PostMapping
+    public ResponseEntity generatePutPresignedURL(@RequestParam MultipartFile file) throws IOException {
+        Regions clientRegion = Regions.AP_NORTHEAST_2;
+        String bucketName = "bucket-deploy-challenge";
+        String objectKey = "upload/" + file.getOriginalFilename();
+
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withRegion(clientRegion)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+
+        s3Client.putObject(bucketName, objectKey, imageService.convertFile(file));
+        //s3Client.deleteObject(bucketName, objectKey);
+
+        // URL 유효시간 24시간으로 설정
+        Date expiration = new Date();
+        long expTimeMillis = Instant.now().toEpochMilli();
+        expTimeMillis += 1000 * 60 * 60 * 24;
+        expiration.setTime(expTimeMillis);
+
+        // URL 발급
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest(bucketName, objectKey)
+                        .withMethod(HttpMethod.PUT)
+                        .withExpiration(expiration);
+        generatePresignedUrlRequest.addRequestParameter(Headers.S3_CANNED_ACL,
+                CannedAccessControlList.PublicRead.toString());
+        String url = s3Client.generatePresignedUrl(generatePresignedUrlRequest).toString();
+
+        return new ResponseEntity(url.toString(), HttpStatus.OK);
+
+    }
+
+    @ApiOperation(value = "2. 이미지파일 정보 DB에 업로드, S3 버킷에 업로드 후 조회를 위한 URL 출력")
     @PutMapping
     public ResponseEntity postImage(@ApiParam(value = "파일 업로드", required = true)
                                         @RequestParam MultipartFile file) {
@@ -48,18 +91,12 @@ public class ImageController {
 
         ImageResponseDto response = mapper.imageToImageResponseDto(image);
 
-        response.setPresignedUrl(generatePresignedUrlGet(response.getFileName()));
+        response.setPresignedUrl(generateGetPresignedUrl(response.getFileName()));
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @Value("${AWS_ACCESS_KEY}")
-    private String accessKey;
-
-    @Value("${AWS_SECRET_KEY}")
-    private String secretKey;
-
-    private String generatePresignedUrlGet(@Nullable String fileName) {
+    private String generateGetPresignedUrl(@Nullable String fileName) {
         Regions clientRegion = Regions.AP_NORTHEAST_2;
         String bucketName = "bucket-deploy-challenge";
         String objectKey = "upload/" + fileName;
@@ -82,40 +119,6 @@ public class ImageController {
                 new GeneratePresignedUrlRequest(bucketName, objectKey)
                         .withMethod(HttpMethod.GET)
                         .withExpiration(expiration);
-        generatePresignedUrlRequest.addRequestParameter(Headers.S3_CANNED_ACL,
-                CannedAccessControlList.PublicRead.toString());
-        URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
-
-        return url.toString();
-    }
-
-    @ApiOperation(value = "presignedURL 획득")
-    @GetMapping
-    public String generatePresignedUrlPost() {
-        Regions clientRegion = Regions.AP_NORTHEAST_2;
-        String bucketName = "bucket-deploy-challenge";
-        String objectKey = "upload/";
-
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                .withRegion(clientRegion)
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .build();
-
-        // URL 유효시간 24시간으로 설정
-        Date expiration = new Date();
-        long expTimeMillis = Instant.now().toEpochMilli();
-        expTimeMillis += 1000 * 60 * 60 * 24;
-        expiration.setTime(expTimeMillis);
-
-        // URL 발급
-        GeneratePresignedUrlRequest generatePresignedUrlRequest =
-                new GeneratePresignedUrlRequest(bucketName, objectKey)
-                        .withMethod(HttpMethod.GET)
-                        .withExpiration(expiration);
-        generatePresignedUrlRequest.addRequestParameter(Headers.S3_CANNED_ACL,
-                CannedAccessControlList.PublicRead.toString());
         URL url = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
 
         return url.toString();
